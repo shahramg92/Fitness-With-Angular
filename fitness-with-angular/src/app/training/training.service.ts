c7756452eb280c1b732bf13fc1bc40756627656b
Shahram Ghassemi <shahramg92@hotmail.com>
2018-09-14 01:48:51 -0500

    updates

--- fitness-with-angular/src/app/training/training.service.ts
+++ fitness-with-angular/src/app/training/training.service.ts
@@ -1,10 +1,28 @@
+import { Subject } from 'rxjs';
+
 import { Exercise } from './exercise.model';
 
 export class TrainingService {
-  availableExercises: Exercise[] = [
+  exerciseChanged = new Subject<Exercise>();
+  private availableExercises: Exercise[] = [
     { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
     { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
     { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18 },
     { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 }
   ];
+  private runningExercise: Exercise;
+
+  getAvailableExercises() {
+    return this.availableExercises.slice();
+  }
+
+  startExercise(selectedId: string) {
+    const selectedExercise = this.availableExercises.find(ex => ex.id === selectedId);
+    this.runningExercise = selectedExercise;
+    this.exerciseChanged.next({...this.runningExercise});
+  }
+
+  getRunningExercise() {
+    return {...this.runningExercise};
+  }
 }
