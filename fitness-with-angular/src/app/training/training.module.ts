84b9b57dfaec73d84cdc40f53561a6e711220bf4
Shahram Ghassemi <shahramg92@hotmail.com>
2018-09-18 00:34:19 -0500

    added shared module

--- fitness-with-angular/src/app/training/training.module.ts
+++ fitness-with-angular/src/app/training/training.module.ts
@@ -1,7 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 import { TrainingComponent } from './training.component';
@@ -9,7 +6,7 @@ import { CurrentTrainingComponent } from './current-training/current-training.co
 import { NewTrainingComponent } from './new-training/new-training.component';
 import { PastTrainingsComponent } from './past-trainings/past-trainings.component';
 import { StopTrainingComponent } from './current-training/stop-training.component';
-import { MaterialModule } from '../material.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -20,10 +17,7 @@ import { MaterialModule } from '../material.module';
     StopTrainingComponent
   ],
   imports: [
-    CommonModule,
-    FormsModule,
-    MaterialModule,
-    FlexLayoutModule,
+    SharedModule,
     AngularFirestoreModule
   ],
   entryComponents: [StopTrainingComponent]
